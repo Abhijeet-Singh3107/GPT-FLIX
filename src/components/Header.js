558dcd6f7c1a70385f69290d69bb69c77e82cb30
= <=>
2025-09-27 18:12:53 +0530

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    // unsubscribe when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div className="absolute top-0 left-0 w-full z-20">
+    <div className="absolute top-0 left-0 w-full z-20 flex justify-between items-center">
       {/* Gradient background */}
       <div className="absolute inset-0 h-20 bg-gradient-to-b from-black/70 to-transparent"></div>
 
@@ -10,10 +50,27 @@ const Header = () => {
       <div className="relative px-8 py-2">
         <img
           className="w-44"
-          src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src= {LOGO}
           alt="netflix-logo"
         />
       </div>
+
+      {/* User + Sign Out */}
+      {user && (
+        <div className="relative flex items-center gap-4 px-8">
+          <img
+            className="w-10 h-10 rounded-md border border-gray-400"
+            src={user?.photoURL || "/userLogo.png"}
+            alt="userLogo"
+          />
+          <button
+            onClick={handleSignOut}
+            className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700 transition-colors"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
