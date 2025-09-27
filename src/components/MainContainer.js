cdd1512c1336223931f17e1c65e6a9e95659dc00
= <=>
2025-09-27 23:24:09 +0530

    worked on the movies list && custom hooks

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,13 +9,18 @@ const MainContainer = () => {
   // console.log(mainMovie);
   const { original_title, title, overview, id } = mainMovie;
   return (
-    <div className="relative w-full h-screen">
-      <VideoTitle
-        orgTitle={original_title}
-        engTitle={title}
-        overview={overview}
-      />
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Background Video */}
       <VideoBackground movieId={id} />
+
+      {/* Overlay Text */}
+      <div className="absolute inset-0 flex flex-col justify-center px-12 text-white bg-gradient-to-r from-black/70 to-transparent">
+        <VideoTitle
+          orgTitle={original_title}
+          engTitle={title}
+          overview={overview}
+        />
+      </div>
     </div>
   );
 };
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,16 +1,14 @@
-
 import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full overflow-hidden -z-10">
+    <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
       <iframe
-        className="w-full h-full object-cover"
+        className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto transform -translate-x-1/2 -translate-y-1/2"
         src={
           "https://www.youtube.com/embed/" +
           trailerVideo?.key +
@@ -25,8 +23,8 @@ const VideoBackground = ({ movieId }) => {
       {/* top gradient for text readability */}
       <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-60"></div>
 
-      {/* bottom fade-to-black gradient */}
-      <div className="absolute bottom-0 w-full h-40 bg-gradient-to-t from-black to-transparent"></div>
+      {/* bottom fade-to-black gradient + safe padding for subtitles */}
+      <div className="absolute bottom-0 w-full h-52 bg-gradient-to-t from-black to-transparent"></div>
     </div>
   );
 };
