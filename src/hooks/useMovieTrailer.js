52ce1d253ec720e5388697c3c38a8ffa297afd86
= <=>
2025-10-03 00:21:06 +0530

    used the concept of memoisation in this, to reduce the number of API calls

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,10 +1,13 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useMovieTrailer = ( movieId ) => {
   const dispatch = useDispatch();
+
+  const checkTrailerVideo = useSelector((store) => store.movies.trailerVideo);
+
   // fet movie trailer and update the store with trailer video
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -26,7 +29,7 @@ const useMovieTrailer = ( movieId ) => {
   };
 
   useEffect(() => {
-    getMovieVideos();
+    !checkTrailerVideo && getMovieVideos();
   }, []);
 };
 
